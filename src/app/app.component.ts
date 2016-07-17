344c4a5f5087c1a88159ca9ca7c21d2eb6de1be5
Dvir Hazut <dvir.hazut@kaltura.com>
2016-07-17 16:32:03 +0300

    load more basic function, moving data into event grid infinite. move event-grid into event-grid-infinite in order to use the data from it to populate event-grid.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { NavbarComponent } from './navbar';
-import { EventGridComponent } from './event-grid';
 import { EventGridInfiniteComponent } from './event-grid-infinite';
-
-import { EventModel } from './models/event';
 
 @Component({
   moduleId: module.id,
@@ -11,45 +8,8 @@ import { EventModel } from './models/event';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   encapsulation: ViewEncapsulation.None,
-  directives: [NavbarComponent, EventGridComponent, EventGridInfiniteComponent]
+  directives: [NavbarComponent, EventGridInfiniteComponent]
 })
 export class AppComponent {
-  events: Array<EventModel> = new Array();
-  constructor() {
-    let sampleEvent = new EventModel({
-      title: 'Angular 2',
-      description: 'intro to Angular 2',
-      group: {
-        slug: 'angular-js',
-        name: 'kNG2',
-        createdOn: 123
-      },
-      location: 'Conf-TLV',
-      createdOn: 123,
-      organizer: {
-        firstName: 'dvir',
-        lastName: 'hazout',
-        email: 'dvir.hazut@kaltura.com'
-      },
-      startTime: 123,
-      endTime: 123,
-      attending: {
-        total: 10,
-        users: [{
-          firstName: 'dvir',
-          lastName: 'hazout',
-          email: 'dvir.hazut@kaltura.com'
-        },
-        {
-          firstName: 'dvir',
-          lastName: 'hazout',
-          email: 'dvir.hazut@kaltura.com'
-        }]
-      }
-    });
-    this.events.push(sampleEvent);
-    this.events.push(sampleEvent);
-    this.events.push(sampleEvent);
-    this.events.push(sampleEvent);
-  }
+  
 }
--- src/app/event-grid-infinite/event-grid-infinite.component.ts
+++ src/app/event-grid-infinite/event-grid-infinite.component.ts
@@ -1,16 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { EventGridComponent } from '../event-grid';
+import { EventModel } from '../models/event';
+
+var sampleEvent = new EventModel({
+  title: 'Angular 2',
+  description: 'intro to Angular 2',
+  group: {
+    slug: 'angular-js',
+    name: 'kNG2',
+    createdOn: 123
+  },
+  location: 'Conf-TLV',
+  createdOn: 123,
+  organizer: {
+    firstName: 'dvir',
+    lastName: 'hazout',
+    email: 'dvir.hazut@kaltura.com'
+  },
+  startTime: 123,
+  endTime: 123,
+  attending: {
+    total: 10,
+    users: [{
+      firstName: 'dvir',
+      lastName: 'hazout',
+      email: 'dvir.hazut@kaltura.com'
+    },
+    {
+      firstName: 'dvir',
+      lastName: 'hazout',
+      email: 'dvir.hazut@kaltura.com'
+    }]
+  }
+});
 
 @Component({
   moduleId: module.id,
   selector: 'app-event-grid-infinite',
   templateUrl: 'event-grid-infinite.component.html',
-  styleUrls: ['event-grid-infinite.component.css']
+  styleUrls: ['event-grid-infinite.component.css'],
+  directives: [EventGridComponent]
 })
 export class EventGridInfiniteComponent implements OnInit {
+  events: Array<EventModel> = new Array();
 
-  constructor() {}
+  constructor() {
+    this.events.push(sampleEvent);
+    this.events.push(sampleEvent);
+    this.events.push(sampleEvent);
+    this.events.push(sampleEvent);
+  }
 
   ngOnInit() {
+  }
+
+  loadMore() {
+    this.events.push(sampleEvent);
+    this.events.push(sampleEvent);
   }
 
 }
